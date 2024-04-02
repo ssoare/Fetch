db7e2de027613485c500f8f8e4b6b4152320d7ea
ssoare <154654902+ssoare@users.noreply.github.com>
2024-04-02 01:03:03 +0000

    added types

--- fetch/src/App.jsx
+++ fetch/src/App.jsx
@@ -2,35 +2,37 @@ import { useState, useEffect } from 'react'
 
 function App() {
   const [pokemon, setPokemon] = useState([])
-  const [type, setType] = useState(['poison'])
+  const [type, setType] = useState([])
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon')
       .then(res => res.json())
       .then(data => {
-        setPokemon(data.results.map(poke => poke.name))
+        const pokemonPromises = data.results.map(poke =>
+          fetch(`https://pokeapi.co/api/v2/pokemon/${poke.name}`)
+            .then(res => res.json())
+        );
+        Promise.all(pokemonPromises)
+          .then(pokemonDetails => setPokemon(pokemonDetails));
       })
   }, [])
+  
 
   return (
     <>
       <section className='flex flex-wrap gap-8 p-4'>
-          {pokemon.map((poke, index) => {
-            return(
-              <>
-                  <article key={index} className='border-2 w-60 p-4 rounded-xl shadow-md shadow-black group transition cursor-pointer'>
-                    <img className='size-40 drop-shadow-soare group-hover:scale-150 transition mx-auto mb-8' 
-                      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + 1}.svg`} 
-                      alt={`Imagen de ${poke}`} />
-                    <h2 className='capitalize text-center text-xl font-semibold group-hover:text-red-500 transition'>#{index + 1}</h2>
-                    <h2 className='capitalize text-center text-xl font-semibold group-hover:text-red-500 transition'>{poke}</h2>
-                    <p>{type}</p>
-                  </article>
-              </>
-            )
-          })}
+        {pokemon.map((poke, index) => (
+          <article key={index} className='border-2 w-60 p-4 rounded-xl shadow-md shadow-black group transition cursor-pointer'>
+            <img className='size-40 drop-shadow-soare group-hover:scale-150 transition mx-auto mb-12' 
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + 1}.svg`} 
+              alt={`Imagen de ${poke}`} />
+            <h2 className='capitalize text-center text-xl font-semibold group-hover:text-red-500 transition'>#{index + 1} <span className='capitalize'>{poke.name}</span>
+            </h2>
+            <p className='text-center italic text-gray-500'>{poke.types ? poke.types.map(type => type.type.name).join(', ') : 'No types found'}</p>
+          </article>
+        ))}
       </section>
     </>
   )
-}
+}  
 
 export default App
--- fetch/src/components/header.jsx
+++ fetch/src/components/header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <>
             <header className="fixed top-0 w-full h-12 flex justify-center text-xl items-center z-10 bg-teal-200 font-semibold">
-                <h1>PokéDex</h1>
+                <a href="/"><h1>PokéDex</h1></a>
             </header>
         </>
     )
